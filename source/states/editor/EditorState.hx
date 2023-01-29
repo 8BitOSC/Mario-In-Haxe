e9d4539bf03342a753aff99b3546123b2b1ab6fa
8BitJake <jake8bittaylor@gmail.com>
2023-01-29 11:16:52 -0600

    undo lots of stuff

--- source/states/editor/EditorState.hx
+++ source/states/editor/EditorState.hx
@@ -7,38 +7,35 @@ import flixel.addons.display.FlxGridOverlay;
 import states.State;
 import flixel.FlxSprite;
 import sprites.TileGroup;
-import flixel.group.FlxGroup;
-import flixel.util.FlxColor;
-import flixel.util.FlxTimer;
 
 class EditorState extends State {
+	var tiles:TileGroup = new TileGroup({
+		tiles: [for (i in 0...50) {x: i, y: 0, type: 'ground'}].concat([for (i in 0...50) {x: i, y: 1, type: 'ground'}]),
+		theme: "ground",
+		scale: 2
+	});
 	var selectedTile:Tile = null;
 
 	var levelInfo:LevelMeta = {
-		tiles: [for (i in 0...50) {x: i, y: 0, type: 'ground'}].concat([for (i in 0...50) {x: i, y: 1, type: 'ground'}]),
+		tiles: [],
 		theme: "ground",
 		scale: 2
 	}
 
 	var tileSize:Float = 16;
 
-	var tiles:TileGroup = null;
-
 	override public function create():Void {
 		super.create();
-		// bgColor = 0xff8f9aff;
+		bgColor = 0xff8f9aff;
 
 		FlxG.mouse.visible = true;
 		FlxG.mouse.useSystemCursor = true;
 
 		tileSize = 16 * levelInfo.scale;
 
-		tiles = new TileGroup(levelInfo);
-
-		add(tiles);
-
 		selectedTile = new Tile(0, 0, levelInfo.scale);
 		selectedTile.alpha = 0.4;
+		add(selectedTile);
 
 		FlxG.watch.add(FlxG.camera, 'zoom', 'zoom');
 		FlxG.watch.add(FlxG.camera.scroll, 'x', 'scrollX');
@@ -46,7 +43,7 @@ class EditorState extends State {
 		FlxG.watch.add(cameraBounds, 'width', 'cameraWidth');
 		FlxG.watch.add(cameraBounds, 'height', 'cameraHeight');
 
-		add(selectedTile);
+		add(tiles);
 	}
 
 	override public function update(elapsed:Float):Void {
